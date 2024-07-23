4e742f1472e72a9702bc89c54655799b972c93c0
rajputaadi <139755855+rajputaadi@users.noreply.github.com>
2024-07-23 10:57:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.cpp
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data%10; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/* 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    public:
+    
+    int addhelper(Node*temp){
+        if(temp==NULL) return 1;
+        int carry = addhelper(temp->next);
+        if(temp->data + carry < 10){
+            temp->data += carry;
+            return 0;
+        }
+        else{
+            temp->data = 0;
+            return 1;
+        }
+    }
+    Node* addOne(Node *head) 
+    {
+        int carry = addhelper(head);
+        if(carry==1){
+            Node* newnode =  new Node(1);
+            newnode->next = head;
+            head = newnode;
+        }
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        
+        Node* head = new Node( s[0]-'0' );
+        Node* tail = head;
+        for(int i=1; i<s.size(); i++)
+        {
+            tail->next = new Node( s[i]-'0' );
+            tail = tail->next;
+        }
+        Solution ob;
+        head = ob.addOne(head);
+        printList(head); 
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
