44787997dd72ba05475fc2d23b624fc4eb042857
rajputaadi <139755855+rajputaadi@users.noreply.github.com>
2024-08-23 11:08:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int findKRotation(vector<int> &arr) {
+        int n = arr.size();
+        int low = 0;
+        int index =-1;
+        int high = n-1;
+        int min = INT_MAX;
+        while(low<=high){
+            if(arr[low]<=arr[high]){
+                if(arr[low]<min){
+                    min = arr[low];
+                    index = low;
+                }
+                low++;
+                high--;
+            }
+            else{
+                if(arr[high] < min){
+                    min = arr[high];
+                    index = high;
+                }
+                high--;
+                low++;
+            }
+        }
+        return index;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input1;
+        getline(cin, input1);
+        stringstream ss1(input1);
+        int number1;
+        while (ss1 >> number1) {
+            arr.push_back(number1);
+        }
+        Solution ob;
+        int res = ob.findKRotation(arr);
+        cout << res << endl;
+    }
+}
+// } Driver Code Ends
